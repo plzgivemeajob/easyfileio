60380f082d9291d8407f467f5c3eaf673f1be5f0
plzgivemeajob <zoosline@outlook.jp>
2025-07-24 15:53:43 +0900

    what am i doing

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='easyfileio',
-    version=1.1,
+    version=1.3,
     packages=find_packages(),
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
